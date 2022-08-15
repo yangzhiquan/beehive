b4698f2077a677fbe93fd130e9069c3213f843bb
yvanyang <yvanyang@tencent.com>
2022-08-15 20:24:08 +0800

    剪切板监听

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,8 +3,14 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  var statusBar: StatusBarController?
-  var popover = NSPopover.init()
+    var statusBar: StatusBarController?
+    var popover = NSPopover.init()
+    
+    // clipboard
+    var timer: Timer!
+    let pasteboard: NSPasteboard = .general
+    var lastChangeCount: Int = 0
+
     
     override init() {
       popover.behavior = NSPopover.Behavior.transient //to make the popover hide when the user clicks outside of it
@@ -15,6 +21,15 @@ class AppDelegate: FlutterAppDelegate {
 
     override func applicationWillFinishLaunching(_ notification: Notification) {
         mainFlutterWindow.close()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { (t) in
+            if self.lastChangeCount != self.pasteboard.changeCount {
+                self.lastChangeCount = self.pasteboard.changeCount
+//                    NotificationCenter.default.post(name: .NSPasteboardDidChange, object: self.pasteboard)
+                print(self.pasteboard.getPlainString() ?? "none")
+            }
+        }
+        
     }
     
     override func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -23,6 +38,24 @@ class AppDelegate: FlutterAppDelegate {
       popover.contentSize = NSSize(width: 360, height: 360) //change this to your desired size
       popover.contentViewController = controller //set the content view controller for the popover to flutter view controller
       statusBar = StatusBarController.init(popover)
-      super.applicationDidFinishLaunching(aNotification)
+//      super.applicationDidFinishLaunching(aNotification)
+    }
+    
+    override func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+        timer.invalidate()
+    }
+}
+
+
+//extension NSNotification.Name {
+//    public static let NSPasteboardDidChange: NSNotification.Name = .init(rawValue: "pasteboardDidChangeNotification")
+//}
+
+
+extension NSPasteboard {
+    func getPlainString() -> String? {
+        guard let data = data(forType: .string) else { return nil }
+        return String(data: data, encoding: .utf8)
     }
 }
