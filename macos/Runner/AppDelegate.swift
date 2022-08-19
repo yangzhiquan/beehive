98059ea40b73d288b3c3d6dd51272063f73ebbfb
yvanyang <mail4yangzq@gmail.com>
2022-08-20 00:40:25 +0800

    fix window size

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: FlutterAppDelegate {
         if let visibleFrame = NSScreen.main?.visibleFrame, let frame = NSScreen.main?.frame {
             let windowWidth = 360
             
-            mainFlutterWindow.setFrame(NSRect(x: Int(frame.size.width - CGFloat(windowWidth)), y: Int(visibleFrame.origin.y), width: windowWidth, height: Int(visibleFrame.size.height)), display: true)
+            mainFlutterWindow.setFrame(NSRect(x: Int(frame.size.width - CGFloat(windowWidth)), y: Int(visibleFrame.origin.y), width: windowWidth, height: Int(frame.size.height - visibleFrame.origin.y)), display: true)
         }
         statusBar = StatusBarController.init(mainFlutterWindow)
     }
