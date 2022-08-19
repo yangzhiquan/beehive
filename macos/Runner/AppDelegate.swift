310d488d21fc470f3bc6a9c745181046c095974e
yvanyang <yvanyang@tencent.com>
2022-08-19 20:27:27 +0800

    update window's default position

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -9,6 +9,14 @@ class AppDelegate: FlutterAppDelegate {
       return false
     }
     
+    override func applicationWillFinishLaunching(_ notification: Notification) {
+        if let visibleFrame = NSScreen.main?.visibleFrame, let frame = NSScreen.main?.frame {
+            let windowWidth = 360
+            
+            mainFlutterWindow.setFrame(NSRect(x: Int(frame.size.width - CGFloat(windowWidth)), y: Int(visibleFrame.origin.y), width: windowWidth, height: Int(visibleFrame.size.height)), display: true)
+        }
+    }
+    
     override func applicationDidFinishLaunching(_ aNotification: Notification) {
 
     }
