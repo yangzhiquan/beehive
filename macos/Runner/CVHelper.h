90f7fd188594a498db9ed248eb9795d9c885ccf5
yvanyang <yvanyang@tencent.com>
2022-08-25 12:46:17 +0800

    引入OpenCV

---
+++ macos/Runner/CVHelper.h
@@ -0,0 +1,16 @@
+//
+//  CVHelper.h
+//  Runner
+//
+//  Created by YvanYang on 2022/8/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CVHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ macos/Runner/CVHelper.mm
@@ -0,0 +1,16 @@
+//
+//  CVHelper.m
+//  Runner
+//
+//  Created by YvanYang on 2022/8/25.
+//
+
+
+#ifdef __cplusplus
+#import <opencv2/opencv.hpp>
+#endif
+#import "CVHelper.h"
+
+@implementation CVHelper
+
+@end
